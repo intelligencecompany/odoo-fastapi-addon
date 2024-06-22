088b3853303985aeef06d0fcd0176464b37b7ff9
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-22 20:55:33 +0200

    dyna

--- controllers/fastapi_server.py
+++ controllers/fastapi_server.py
@@ -16,6 +16,16 @@ app = FastAPI()
 
 api_key_header = APIKeyHeader(name='x-key')
 
+
+async def get_model(model: str):
+    api_key = 'admin'
+    uid, models = get_connection(api_key)
+    if uid:
+        partners = models.execute_kw(ODOO_DB, uid, api_key, model, 'search_read', [[]])
+        return json.dumps(partners)
+    else:
+        return json.dumps({'status': 'Connection failed'})
+    
 async def create_dynamic_endpoint(model: str, method: str):
     method = method.upper()
     if method == 'GET':
@@ -31,7 +41,7 @@ def add_api_routes():
 
     for n in model_names[:2]:
         logging.info(n)
-        app.add_api_route(f'api/{n.model}', create_dynamic_endpoint(n.model, 'GET'), methods=['GET'])
+        app.add_api_route(f'/api/{n.model}', create_dynamic_endpoint(n.model, 'GET'), methods=['GET'])
 
 # XML-RPC connection
 def get_connection(api_key: str):
@@ -40,33 +50,35 @@ def get_connection(api_key: str):
     models = xmlrpc.client.ServerProxy(f'{ODOO_URL}/xmlrpc/2/object')
     return uid, models
 
-@app.get("/api/test")
-async def test_connection(api_key:str = Depends(api_key_header)):
-    # api_key = 'admin'
-    uid, models = get_connection(api_key)
-    if uid:
-        return json.dumps({'status': 'Connection successful', 'uid': uid })
-    else:
-        return json.dumps({'status': 'Connection failed' })
+add_api_routes()
 
-@app.get("/api/models")
-async def get_models(api_key:str = Depends(api_key_header)):
-    uid, models = get_connection(api_key)
-    if uid:
-        model_ids = models.execute_kw(ODOO_DB, uid, api_key, 'ir.model', 'search', [[]])
-        model_names = models.execute_kw(ODOO_DB, uid, api_key, 'ir.model', 'read', [model_ids, ['model', 'name']])
+# @app.get("/api/test")
+# async def test_connection(api_key:str = Depends(api_key_header)):
+#     # api_key = 'admin'
+#     uid, models = get_connection(api_key)
+#     if uid:
+#         return json.dumps({'status': 'Connection successful', 'uid': uid })
+#     else:
+#         return json.dumps({'status': 'Connection failed' })
 
-        return json.dumps(model_names)
-    else:
-        return json.dumps({'status': 'Connection failed'})
+# @app.get("/api/models")
+# async def get_models(api_key:str = Depends(api_key_header)):
+#     uid, models = get_connection(api_key)
+#     if uid:
+#         model_ids = models.execute_kw(ODOO_DB, uid, api_key, 'ir.model', 'search', [[]])
+#         model_names = models.execute_kw(ODOO_DB, uid, api_key, 'ir.model', 'read', [model_ids, ['model', 'name']])
 
-@app.get("/api/{model}")
-async def get_model(model: str, api_key:str = Depends(api_key_header)):
-    uid, models = get_connection(api_key)
-    if uid:
-        partners = models.execute_kw(ODOO_DB, uid, api_key, model, 'search_read', [[]])
-        return json.dumps(partners)
-    else:
-        return json.dumps({'status': 'Connection failed'})
+#         return json.dumps(model_names)
+#     else:
+#         return json.dumps({'status': 'Connection failed'})
+
+# @app.get("/api/{model}")
+# async def get_model(model: str, api_key:str = Depends(api_key_header)):
+#     uid, models = get_connection(api_key)
+#     if uid:
+#         partners = models.execute_kw(ODOO_DB, uid, api_key, model, 'search_read', [[]])
+#         return json.dumps(partners)
+#     else:
+#         return json.dumps({'status': 'Connection failed'})
 
 # add_api_routes()
