c67082cae776854ed7c5e7234157c9dbf1d39c94
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-22 14:30:34 +0200

    openapi

--- controllers/fastapi_controller.py
+++ controllers/fastapi_controller.py
@@ -4,19 +4,25 @@ import requests
 import json
 
 class FastApiController(http.Controller):
+    @http.route('/openapi.json', auth='public')
+    def openapi(self):
+        url = 'http://127.0.0.1:8000/openapi.json'
+        response = requests.get(url)
+        return response.content
+    
     @http.route('/api/docs', auth='public')
     def docs(self):
         url = 'http://127.0.0.1:8000/docs'
         response = requests.get(url)
-        return response
+        return response.content
     
-    @http.route('/api/test', type='json', auth='public')
+    @http.route('/api/test', auth='public')
     def test(self):
         url = 'http://127.0.0.1:8000/test'
         response = requests.get(url)
         return response.text
     
-    @http.route('/api/partners', type='json', auth='public')
+    @http.route('/api/partners', auth='public')
     def parnters(self):
         url = 'http://127.0.0.1:8000/partners'
         response = requests.get(url)
