e92eba0fc0b938aa125af1ffae0de0c3a8105dda
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-22 14:15:36 +0200

    routes

--- controllers/fastapi_controller.py
+++ controllers/fastapi_controller.py
@@ -4,19 +4,19 @@ import requests
 
 class FastApiController(http.Controller):
     @http.route('/api/docs', auth='public')
-    def handler(self):
+    def docs(self):
         url = 'http://localhost:8000/docs'
         response = requests.get(url)
         return response
     
     @http.route('/api/test', auth='public')
-    def handler(self):
+    def test(self):
         url = 'http://localhost:8000/test'
         response = requests.get(url)
         return response.json()
     
-    @http.route('/api/partners', auth='public')
-    def handler(self):
+    @http.route('/api/partners', type='json', auth='public')
+    def parnters(self):
         url = 'http://127.0.0.1:8000/partners'
         response = requests.get(url)
-        return { 'url': url, 'status_code': response.status_code }
+        return response.json()
