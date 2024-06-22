3bbc4353322fc7176cc9429d2c47a476d02bce9f
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-22 20:50:59 +0200

    dyna

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,3 +1,3 @@
 from . import fastapi_server
 from . import fastapi_controller
-from . import dynamic_controller
+# from . import dynamic_controller
--- controllers/fastapi_controller.py
+++ controllers/fastapi_controller.py
@@ -17,47 +17,47 @@ class FastApiController(http.Controller):
         response = requests.get(url)
         return response.content
     
-    @http.route('/api/test', methods=['GET'], auth='public')
-    def test(self):
-        try:            
-            x_key_header = http.request.httprequest.headers.get('x-key')
-            headers = {
-                'x-key': x_key_header
-            }
+    # @http.route('/api/test', methods=['GET'], auth='public')
+    # def test(self):
+    #     try:            
+    #         x_key_header = http.request.httprequest.headers.get('x-key')
+    #         headers = {
+    #             'x-key': x_key_header
+    #         }
 
-            url = 'http://127.0.0.1:8000/api/test'
-            response = requests.get(url=url, headers=headers)
-            response.raise_for_status()
-            # Parse the response content as JSON
-            response_json = response.json()
-            # Return the JSON response
-            return response_json
-        except requests.exceptions.RequestException as e:
-            logging.error(e)
-            response_data = {'error': str(e)}
-            return json.dumps(response_data)
+    #         url = 'http://127.0.0.1:8000/api/test'
+    #         response = requests.get(url=url, headers=headers)
+    #         response.raise_for_status()
+    #         # Parse the response content as JSON
+    #         response_json = response.json()
+    #         # Return the JSON response
+    #         return response_json
+    #     except requests.exceptions.RequestException as e:
+    #         logging.error(e)
+    #         response_data = {'error': str(e)}
+    #         return json.dumps(response_data)
         
-    @http.route('/api/models', methods=['GET'], auth='public')
-    def models(self):       
-        x_key_header = http.request.httprequest.headers.get('x-key')
-        headers = {
-            'x-key': x_key_header
-        }
-        url = 'http://127.0.0.1:8000/api/models'
-        response = requests.get(url=url, headers=headers)
-        # Parse the response content as JSON
-        response_json = response.json()
-        # Return the JSON response
-        return response_json
+    # @http.route('/api/models', methods=['GET'], auth='public')
+    # def models(self):       
+    #     x_key_header = http.request.httprequest.headers.get('x-key')
+    #     headers = {
+    #         'x-key': x_key_header
+    #     }
+    #     url = 'http://127.0.0.1:8000/api/models'
+    #     response = requests.get(url=url, headers=headers)
+    #     # Parse the response content as JSON
+    #     response_json = response.json()
+    #     # Return the JSON response
+    #     return response_json
     
-    @http.route('/api/<string:model>', methods=['GET'], auth='public')
-    def model(self, model=None):
+    @http.route('/api/<string:action>', methods=['GET'], auth='public')
+    def model(self, action=None):
         x_key_header = http.request.httprequest.headers.get('x-key')
         headers = {
             'x-key': x_key_header
         }
 
-        url = f'http://127.0.0.1:8000/api/{model}'
+        url = f'http://127.0.0.1:8000/api/{action}'
         response = requests.get(url=url, headers=headers)
         # Parse the response content as JSON
         response_json = response.json()
