3b176ec9c4317cddc3d5670f527d7d765449c80d
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-26 18:44:29 +0200

    multi

--- models/openapi_model.py
+++ models/openapi_model.py
@@ -1,5 +1,5 @@
 # models/res_users.py
-from odoo import models, fields, api
+from odoo import models, fields
 
 class FastApiModel(models.Model):
     _name = 'openapi.model'
@@ -8,17 +8,14 @@ class FastApiModel(models.Model):
     name = fields.Char(string='Name')
     fastapi_url = fields.Char(string="FASTAPI URL")
 
-    @api.multi
     def action_button_1(self):
         # Define action for button 1
         pass
 
-    @api.multi
     def action_button_2(self):
         # Define action for button 2
         pass
 
-    @api.multi
     def action_button_3(self):
         # Define action for button 3
         pass
