7fd68a71ff8788c9040663197e13b85a85a4ebad
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-26 13:05:16 +0200

    update

--- controllers/fastapi_controller.py
+++ controllers/fastapi_controller.py
@@ -40,52 +40,48 @@ class FastApiController(http.Controller):
     
     @http.route('/api/<string:action>/<int:id>', type='http', methods=['PUT'], auth='public', csrf=False)
     def update_record(self, action:str=None, id:int=-1):
+        if http.request.httprequest.content_type != 'application/json':
+            return http.request.make_response(
+                json.dumps({'error': 'Invalid Content-Type'}),
+                headers={'Content-Type': 'application/json'}, 
+                status=400
+            )
+        
         try:
-            if http.request.httprequest.content_type != 'application/json':
-                return http.request.make_response(
-                    json.dumps({'error': 'Invalid Content-Type'}),
-                    headers={'Content-Type': 'application/json'}, 
-                    status=400
-                )
-            
-            try:
-                data = json.loads(http.request.httprequest.data)
-            except json.JSONDecodeError:
-                return http.request.make_response(
-                    json.dumps({'error': 'Invalid JSON data'}),
-                    headers={'Content-Type': 'application/json'}, 
-                    status=400
-                )
+            data = json.loads(http.request.httprequest.data)
+        except json.JSONDecodeError:
+            return http.request.make_response(
+                json.dumps({'error': 'Invalid JSON data'}),
+                headers={'Content-Type': 'application/json'}, 
+                status=400
+            )
 
 
-            if not id or id < 0 or not data:
-                return http.request.make_response(
-                    json.dumps({'error': 'Missing id or fields'}),
-                    headers={'Content-Type': 'application/json'}, 
-                    status=400
-                )
-                                    
-            params = {k: v for k, v in http.request.params.items() if k != 'action' and k != 'id'}
-            headers = {
-                'x-key': http.request.httprequest.headers.get('x-key')
-            }
+        if not id or id < 0 or not data:
+            return http.request.make_response(
+                json.dumps({'error': 'Missing id or fields'}),
+                headers={'Content-Type': 'application/json'}, 
+                status=400
+            )
+                                
+        params = {k: v for k, v in http.request.params.items() if k != 'action' and k != 'id'}
+        headers = {
+            'x-key': http.request.httprequest.headers.get('x-key')
+        }
 
-            url = f'http://127.0.0.1:8000/api/{action}/{id}'
-            response = requests.put(url=url, headers=headers, params=params, data=data)
-            
-            if response.status_code == 200:
-                return http.request.make_response(
-                    json.dumps({'success': True}),
-                    headers={'Content-Type': 'application/json'}, 
-                    status=200
-                )
-            else: 
-                return http.request.make_response(
-                    json.dumps({'error': 'Whoops, something failed'}),
-                    headers={'Content-Type': 'application/json'}, 
-                    status=response.status_code
-                )
-        except Exception as e:
-            logging.error(e)
-            print(e)
+        url = f'http://127.0.0.1:8000/api/{action}/{id}'
+        response = requests.put(url=url, headers=headers, params=params, data=data)
+        
+        if response.status_code == 200:
+            return http.request.make_response(
+                json.dumps({'success': True}),
+                headers={'Content-Type': 'application/json'}, 
+                status=200
+            )
+        else: 
+            return http.request.make_response(
+                json.dumps({'error': response.text}),
+                headers={'Content-Type': 'application/json'}, 
+                status=response.status_code
+            )
 
