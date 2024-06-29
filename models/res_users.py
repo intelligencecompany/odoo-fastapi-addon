ea68f8f8b292cf301137d0b0e3b5ab1bb0eb0355
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-29 15:11:27 +0200

    multi

--- models/res_users.py
+++ models/res_users.py
@@ -8,19 +8,19 @@ class OpenModel(models.Model):
     name = fields.Char('Name')
     description = fields.Text('Description')
 
-    @api.model
+    @api.multi
     def action_button_1(self):
         # Define action for button 1
         self.ensure_one()
         self.name = 'Button 1 Clicked'
 
-    @api.model
+    @api.multi
     def action_button_2(self):
         # Define action for button 2
         self.ensure_one()
         self.name = 'Button 2 Clicked'
 
-    @api.model
+    @api.multi
     def action_button_3(self):
         # Define action for button 3
         self.ensure_one()
