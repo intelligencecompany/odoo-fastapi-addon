c62fe9d5ae197069fcbb1a211627bbd26e7a0c06
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-29 13:05:18 +0200

    x_

--- models/res_users.py
+++ models/res_users.py
@@ -5,8 +5,8 @@ class OpenModel(models.Model):
     _name = 'openapi.model'
     _description = 'OpenAPI Model'
     
-    name = fields.Char(string='Name')
-    description = fields.Text(string='Description')
+    x_name = fields.Char(string='Name')
+    x_description = fields.Text(string='Description')
 
     def action_button_1(self):
         # Define action for button 1
