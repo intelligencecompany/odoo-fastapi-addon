83acdcc26ea62ac0b71284b441d08f7e8d029644
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-29 13:34:45 +0200

    update

--- models/res_users.py
+++ models/res_users.py
@@ -1,25 +1,28 @@
 # models/res_users.py
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class OpenModel(models.Model):
     _name = 'openapi.model'
     _description = 'OpenAPI Model'
         
     name = fields.Char('Name')
-    description = fields.Text('Description')
+    # description = fields.Text('Description')
 
+    @api.model
     def action_button_1(self):
         # Define action for button 1
         self.ensure_one()
         self.name = 'Button 1 Clicked'
         pass
 
+    @api.model
     def action_button_2(self):
         # Define action for button 2
         self.ensure_one()
         self.name = 'Button 2 Clicked'
         pass
 
+    @api.model
     def action_button_3(self):
         # Define action for button 3
         self.ensure_one()
