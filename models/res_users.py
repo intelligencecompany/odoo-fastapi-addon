89c96f90123d64c40752355aba1354f4886c5858
gijs@nlziet.nl <gijs@nlziet.nl>
2024-06-30 10:45:26 +0200

    rm self

--- models/res_users.py
+++ models/res_users.py
@@ -10,22 +10,22 @@ class OpenModel(models.Model):
     description = fields.Text('Description')
     status = fields.Boolean(True)
 
-    def action_button_1(self):
+    def action_button_1():
         # Define action for button 1
-        self.ensure_one()
+        # self.ensure_one()
         fastapi_server.start_fastapi_in_thread()
     # self.status = True
         return True
 
-    def action_button_2(self):
+    def action_button_2():
         # Define action for button 2
-        self.ensure_one()
+        # self.ensure_one()
         fastapi_server.stop_fastapi()
         # self.status = False
         return True
 
-    def action_button_3(self):
+    def action_button_3():
         # Define action for button 3
-        self.ensure_one()
+        # self.ensure_one()
         # self.name = 'Button 3 Clicked'
         return True
